6aeb98b6d2b930f397723fbdfa8ba389caa51f0c
EduardoVc15 <evilela.191ci40@istta.edu.pe>
2021-12-18 22:27:59 -0500

    EAVC

---
+++ resources/views/PaginaPrincipal/PagPrincipal.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.welcome')
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="css/style.css">
+  
+  <title>HLL Inicio</title>
+</head>
+<body>
+
+
+  <section id="reservation" class="py-30">
+    <div class="container">
+      <h2>Make your Reservation Today!</h2>
+      <a href="contact.html" class="btn btn-dark">Make Reservation</a>
+    </div>
+  </section>
+
+  <section id="features">
+    <div class="box features-1">
+      <h4>Double Room</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit sed sunt hic quas eum voluptas saepe nulla nihil harum consectetur.</p>
+    </div>
+    <div class="box features-2">
+      <h4>Deluxe Room</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit sed sunt hic quas eum voluptas saepe nulla nihil harum consectetur.</p>
+    </div>
+    <div class="box features-3">
+      <h4>Honeymoon Room</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit sed sunt hic quas eum voluptas saepe nulla nihil harum consectetur.</p>
+    </div>
+  </section>
+  
+  <div class="clr"></div>
+
+  <footer id="mainfooter" class="py-30">
+    <p>&copy; 2020 Hotel Zante</p>
+  </footer>
+<p>aaaaaaaaaaaaaaaaa</p>
+</body>
+</html>
